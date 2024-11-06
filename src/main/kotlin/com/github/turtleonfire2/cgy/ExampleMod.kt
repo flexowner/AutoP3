fea42391e097e83fe832156e636e871f44d96ffd
GitHub Action <action@github.com>
2024-11-06 13:20:32 +0000

    Initialize template repository proper
    Set package name to com.github.turtleonfire2.cgy
    Set project name to cgy
    Set modid to cgy

--- src/main/kotlin/com/github/turtleonfire2/cgy/ExampleMod.kt
+++ src/main/kotlin/com/github/turtleonfire2/cgy/ExampleMod.kt
@@ -1,4 +1,4 @@
-package com.example
+package com.github.turtleonfire2.cgy
 
 import net.minecraft.client.Minecraft
 import net.minecraft.init.Blocks
@@ -6,7 +6,7 @@ import net.minecraftforge.fml.common.Mod
 import net.minecraftforge.fml.common.event.FMLInitializationEvent
 import net.minecraft.client.renderer.GlStateManager
 
-@Mod(modid = "examplemod", useMetadata = true)
+@Mod(modid = "cgy", useMetadata = true)
 class ExampleMod {
     @Mod.EventHandler
     fun init(event: FMLInitializationEvent) {
--- src/main/kotlin/com/github/turtleonfire2/cgy/mixin/MixinGuiMainMenu.java
+++ src/main/kotlin/com/github/turtleonfire2/cgy/mixin/MixinGuiMainMenu.java
@@ -1,4 +1,4 @@
-package com.example.mixin;
+package com.github.turtleonfire2.cgy.mixin;
 
 import net.minecraft.client.gui.GuiMainMenu;
 import org.spongepowered.asm.mixin.Mixin;
